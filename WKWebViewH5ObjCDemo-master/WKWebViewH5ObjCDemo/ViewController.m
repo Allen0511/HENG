52f8852a9244aee9b9647100cb57fea0ab4950af
Allen0511 <1255268024@qq.com>
2016-11-04 11:34:05 +0800

    888

--- WKWebViewH5ObjCDemo-master/WKWebViewH5ObjCDemo/ViewController.m
+++ WKWebViewH5ObjCDemo-master/WKWebViewH5ObjCDemo/ViewController.m
@@ -26,15 +26,6 @@
   
   WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
   
-  // 设置偏好设置
-  config.preferences = [[WKPreferences alloc] init];
-  // 默认为0
-  config.preferences.minimumFontSize = 10;
-  // 默认认为YES
-  config.preferences.javaScriptEnabled = YES;
-  // 在iOS上默认为NO，表示不能自动通过窗口打开
-  config.preferences.javaScriptCanOpenWindowsAutomatically = NO;
-  
   // web内容处理池
   config.processPool = [[WKProcessPool alloc] init];
   
